4f9d51108726f9dd86caba01b6b925f698133cce
Sami <sami.salonen705@gmail.com>
2018-02-03 13:21:29 +0200

    Procfile added

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-const PORT = process.env.PORT | 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
 })
